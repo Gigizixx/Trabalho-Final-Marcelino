8d3b0308429b123ff17f7d48d57ad6d979ee90f6
Gigizixx <145771652+Gigizixx@users.noreply.github.com>
2023-12-08 01:08:17 -0300

    Atualizar o Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,18 @@
-export default function Home(){
-    return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
-     );
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Header from "./Componentes/Cabeçalho";
+import Card from "./Componentes/Cartão";
+
+export default function Home() {
+  const lista = JSON.parse(localStorage.getItem("Lista"));
+
+  return (
+    <div>
+      <Header />
+      <Card lista={lista} />
+    </div>
+  );
 }
+
+
