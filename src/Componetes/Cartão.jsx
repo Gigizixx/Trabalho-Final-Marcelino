528855db46e70f8735fd1f9f514522a0b622062c
Gigizixx <giovanacaroline455@gmail.com>
2023-12-08 17:23:55 -0400

    08/12/2023

---
+++ src/Componetes/Cartão.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link} from "react-router-dom";
+const Card =({lista}) => {
+    console.log(lista);
+    return (
+        <>
+        {lista}
+        </>
+    )
+}
