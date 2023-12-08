10585c51c6fc830cd5e3e972f74e2717a067f46c
Gigizixx <giovanacaroline455@gmail.com>
2023-12-08 17:59:39 -0400

    08/12/2023

---
+++ src/Detalhe/Detalhe.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../Componentes/Header";
+
+export default function Detalhe() {
+  const { id } = useParams();
+  const lista = JSON.parse(localStorage.getItem("Lista"));
+  const videosertanejo = lista.filter((objeto) => objeto.id == id);
+
+  useEffect(() => {
+    //Exemplo:busca dados adicionais ou executa ações na montagem do componente
+  }, [id]); 
+  return (
+    <div>
+      {videosertanejo.map((video) => (
+        <div key={video.id}>
+          <Header />
+          <iframe
+            title={video.Video}
+            src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
+          
+          ></iframe>
+          <p>{video.Video}</p>
+          <p>{video.famoso}</p>
+          <p>{video.descricao}</p>
+         
+        </div>
+      ))}
+    </div>
+  );
+}
