bdd4b4ddd5599059cfa52627dcb22123b3b8d760
Gigizixx <145771652+Gigizixx@users.noreply.github.com>
2023-12-08 17:30:00 -0400

    Atualizar o Cartão.jsx

--- src/Componetes/Cartão.jsx
+++ src/Componetes/Cartão.jsx
@@ -1,10 +1,24 @@
+
 import React from "react";
-import {Link} from "react-router-dom";
-const Card =({lista}) => {
-    console.log(lista);
-    return (
-        <>
-        {lista}
-        </>
-    )
-}
+import { Link } from "react-router-dom";
+
+const Card = ({ lista }) => {
+  console.log(lista);
+
+  return (
+    <>
+      {lista.map((video) => (
+        <div className="card" key={video.id}>
+          <Link to={`./Detalhe/${video.id}`}>
+            <iframe
+              title={video.Video}
+              src={`adicionar link/embed/${video.url.slice(17)}`}
+              frameBorder="0"
+            ></iframe>
+            <p>{video.Video}</p>
+          </Link>
+        </div>
+      ))}
+    </>
+  );
+};
