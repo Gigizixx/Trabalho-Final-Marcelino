4f77144c9a55d5e9de4318793cbaaead20799bca
Gigizixx <145771652+Gigizixx@users.noreply.github.com>
2023-12-10 01:59:18 -0400

    09/12

--- src/Destaque/Destaque.jsx
+++ src/Destaque/Destaque.jsx
@@ -5,4 +5,14 @@ import { Link } from "react-router-dom";
 import Header from "../Componentes/Cabeçalho";
 import Card from "../Componentes/Cartão";
 
+export default function EmDestaque() {
+  const destaques = JSON.parse(localStorage.getItem("Lista"));
+  const AparecerDestaque = destaques.slice(-3);
 
+  return (
+    <div>
+      <Header />
+      <Card lista={AparecerDestaque} />
+    </div>
+  );
+}
