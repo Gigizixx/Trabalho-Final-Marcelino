87dd58ed48e3f53f9327f9dce8c800a3b0380f3a
Gigizixx <145771652+Gigizixx@users.noreply.github.com>
2023-12-12 14:35:37 -0400

    12/12

--- src/Video/Video.jsx
+++ src/Video/Video.jsx
@@ -4,62 +4,73 @@ import Header from "../Componentes/Cabeçalho";
 import { useNavigate } from "react-router-dom";
 
 export default function Video() {
-  const listaLocalStorage = JSON.parse(localStorage.getItem("Lista")) || [];
-  const [lista, setLista] = useState(listaLocalStorage);
-  const [id, setId] = useState(listaLocalStorage.length ? listaLocalStorage[listaLocalStorage.length - 1].id + 1 : 1);
-  const [Video, setVideo] = useState("");
-  const [linkdovideo, setlinkdovideo] = useState("");
-  const [cantor, setcantor] = useState("");
-  const [descricao, setDescricao] = useState("");
-  const [musicas, setmusicas] = useState("");
-  const navigate = useNavigate();
+    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista")) || [];
+    const [lista, setLista] = useState(listaLocalStorage);
+    const [id, setId] = useState((listaLocalStorage.length && listaLocalStorage[listaLocalStorage.length - 1].id + 1) || 1);
+    const [Video, setVideo] = useState("");
+    const [linkdovideo, setlinkdovideo] = useState("");
+    const [cantor, setcantor] = useState("");
+    const [descricao, setdescricao] = useState("");
+    const [musicas, setmusicas] = useState("");
 
-  useEffect(() => {
-    localStorage.setItem("Lista", JSON.stringify(lista));
-  }, [lista]);
+    useEffect(() => {
+        localStorage.setItem("Lista", JSON.stringify(lista));
+    }, [lista]);
 
-  const salvar = (e) => {
-    e.preventDefault();
-    const novoVideo = {
-      id,
-      Video,
-      linkdovideo,
-      cantor,
-      descricao,
-      musicas,
+    const navigate = useNavigate();
+
+    const salvar = (e) => {
+        e.preventDefault();
+        const Video = {
+            id,
+            Video,
+            linkdovideo,
+            cantor,
+            descricao,
+            musicas,
+        };
+        setLista([...lista, Video]);
+        setId(id + 1);
+        setVideo("");
+        setlinkdovideo("");
+        setcantor("");
+        setdescricao("");
+        setmusicas("");
+        navigate("/");
     };
-    setLista([...lista, novoVideo]);
-    setId(id + 1);
-    setVideo("");
-    setlinkdovideo("");
-    setcantor("");
-    setDescricao("");
-    setmusicas("");
-    navigate("/");
-  };
 
-  return (
-    <div>
-      <Header />
-      <form onSubmit={salvar}>
-        <p>Video:</p>
-        <input type="text" placeholder="Video" value={Video} onChange={(e) => setVideo(e.target.value)} />
+    return (
+        <div>
+            <Header />
 
-        <p>Link do vídeo:</p>
-        <input type="text" placeholder="linkdovidek" value={linkdovideo} onChange={(e) => setLink do video(e.target.value)} />
-        <p>Cantor:</p>
-        <input type="text" placeholder="Cantor" value={cantor} onChange={(e) => setCantor(e.target.value)} />
+            <form onSubmit={salvar}>
+                <label>
+                    Video:
+                    <input type="text" placeholder="Video" value={Video} onChange={(e) => setVideo(e.target.value)} />
+                </label>
 
-        <p>Descrição:</p>
-        <input type="text" placeholder="Descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+                <label>
+                    link do video:
+                    <input type="text" placeholder="linkdovideo" value={linkdovideo} onChange={(e) => setlinkdovideo(e.target.value)} />
+                </label>
 
-        <p>Musicas:</p>
-        <input type="text" placeholder="Musicas" value={musicas} onChange={(e) => setMusicas(e.target.value)} />
+                <label>
+                    Cantor:
+                    <input type="text" placeholder="Cantor" value={cantor} onChange={(e) => setCantor(e.target.value)} />
+                </label>
 
-        <br />
+                <label>
+                    descrição:
+                    <input type="text" placeholder="descricao" value={descricao} onChange={(e) => setdescricao(e.target.value)} />
+                </label>
 
-        <button className="btn btn-outline-dark">ADD</button>
-      </form>
-    </div>
-  );
+                <label>
+                    Musicas:
+                    <input type="text" placeholder="Musicas" value={musicas} onChange={(e) => setMusicas(e.target.value)} />
+                </label>
+
+                <button className="btn btn-outline-dark">ADD</button>
+            </form>
+        </div>
+    );
 }
